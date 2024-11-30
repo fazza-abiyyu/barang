040dbcb461ec56890634d73abea22509f658d4c3
Chiel_Abiyyu <67826596+chi-abiyyu@users.noreply.github.com>
2024-11-30 20:49:23 +0700

    main

--- sql/ci_barang.sql
+++ sql/ci_barang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Nov 2024 pada 10.21
+-- Waktu pembuatan: 30 Nov 2024 pada 14.33
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -41,12 +41,12 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id_barang`, `nama_barang`, `stok`, `satuan_id`, `jenis_id`, `harga_satuan`) VALUES
-('B000001', 'Semen Tiga Roda', 70, 1, 3, 50000.00),
+('B000001', 'Semen Tiga Roda', 200, 1, 3, 50000.00),
 ('B000002', 'Cat Nippon Paint', 1, 1, 4, 100000.00),
 ('B000004', 'Batako Putih', 40, 1, 7, 85000.00),
 ('B000005', 'Batu Kali', 89, 3, 5, 2000.00),
 ('B000006', 'Batu Krosok', 249, 2, 5, 40000.00),
-('B000007', 'Tiner', 50, 4, 4, 10000.00);
+('B000007', 'Tiner', 40, 4, 4, 10000.00);
 
 -- --------------------------------------------------------
 
@@ -73,8 +73,8 @@ CREATE TABLE `barang_keluar` (
 --
 
 INSERT INTO `barang_keluar` (`id_barang_keluar`, `user_id`, `barang_id`, `jumlah_keluar`, `tanggal_keluar`, `satuan_id`, `harga_satuan`, `total_harga`, `pembeli_id`, `stok`, `penerima_type`) VALUES
-('T-BK-24113000001', 1, 'B000005', 1, '2024-11-30', NULL, 2000.00, 2000.00, 4, 90, 'customer'),
-('T-BK-24113000003', 1, 'B000002', 3, '2024-11-30', NULL, 100000.00, 300000.00, 7, 5, 'customer');
+('T-BK-24113000004', 1, 'B000004', 10, '2024-11-22', NULL, 85000.00, 850000.00, 3, 50, 'customer'),
+('T-BK-24113000005', 1, 'B000007', 10, '2024-11-22', NULL, 10000.00, 100000.00, 2, 50, 'customer');
 
 -- --------------------------------------------------------
 
@@ -91,18 +91,16 @@ CREATE TABLE `barang_masuk` (
   `satuan_id` int(11) DEFAULT NULL,
   `harga_satuan` decimal(10,2) NOT NULL,
   `stok` int(11) DEFAULT 0,
-  `id_distributor` int(11) DEFAULT NULL
+  `id_distributor` int(11) DEFAULT NULL,
+  `total_harga` decimal(15,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-ALTER TABLE barang_masuk ADD COLUMN total_harga DECIMAL(15,2);
-
+--
 -- Dumping data untuk tabel `barang_masuk`
 --
 
-INSERT INTO `barang_masuk` (`id_barang_masuk`, `user_id`, `barang_id`, `jumlah_masuk`, `tanggal_masuk`, `satuan_id`, `harga_satuan`, `stok`, `id_distributor`) VALUES
-('T-BM-24113000002', 1, 'B000005', 100, '2024-11-30', NULL, 0.00, 0, NULL),
-('T-BM-24113000003', 1, 'B000006', 50, '2024-11-30', NULL, 0.00, 0, NULL),
-('T-BM-24113000004', 1, 'B000002', 19, '2024-11-30', NULL, 0.00, 0, NULL);
+INSERT INTO `barang_masuk` (`id_barang_masuk`, `user_id`, `barang_id`, `jumlah_masuk`, `tanggal_masuk`, `satuan_id`, `harga_satuan`, `stok`, `id_distributor`, `total_harga`) VALUES
+('T-BM-24113000003', 1, 'B000004', 10, '2024-11-30', NULL, 85000.00, 0, NULL, 850000.00);
 
 -- --------------------------------------------------------
 
@@ -196,7 +194,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama`, `username`, `email`, `no_telp`, `role`, `password`, `created_at`, `foto`, `is_active`) VALUES
-(1, 'Adminisitrator', 'admin', 'admin@admin.com', '025123451234', 'admin', '$2y$10$sXYiyGPKaEZm.hKJPl1uzeBq5fWQF4BFmbGUSVTwHxmkZROg2XoSm', 1568689561, 'e7960a9c81144a253eed1346e09b54f6.jpg', 1);
+(1, 'Adminisitrator', 'admin', 'admin@admin.com', '025123451234', 'admin', '$2y$10$p63r7LMrx6.d5.B.qYedpOmMKt4XQ18JUt0pl/QOgLp9wL7jZ2GUu', 1568689561, 'e7960a9c81144a253eed1346e09b54f6.jpg', 1),
+(4, 'fazza', 'fazza', 'fazza@mail.com', '025123451234', 'gudang', '$2y$10$JdaU8D5tJffh0W5H7SRPZOQ5DzwZ8lsBANTnL8hvdXHbtyccidzNK', 1732965601, 'user.png', 0);
 
 --
 -- Indexes for dumped tables
@@ -276,7 +275,7 @@ ALTER TABLE `satuan`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
